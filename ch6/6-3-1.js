c96449b39a33715d71b293da8e863703c8b83433
taeyi park <ehoi.loveyourself@gmail.com>
2024-08-21 15:02:30 +0900

    3. 최종 리팩토링 버전(변수명만 수정됨)

--- ch6/6-3-1.js
+++ ch6/6-3-1.js
@@ -1,11 +1,6 @@
 export function price(order) {
-  const basicPrice = order.quantity * order.itemPrice;
+  const basePrice = order.quantity * order.itemPrice;
   const discount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-  const deliverFee = Math.min(order.quantity * order.itemPrice * 0.1, 100);
-  return basicPrice - discount + deliverFee;
-  // return (
-  //   order.quantity * order.itemPrice - // 기본가격 
-  //   Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 + // 수량할인
-  //   Math.min(order.quantity * order.itemPrice * 0.1, 100) // 배송비
-  // ); // 각각 변수화해서 가독성을 높이자
+  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+  return basePrice - discount + shipping;
 }
