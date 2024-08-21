a33889319d18f9e26b1d764ee0c213042c74a535
taeyi park <ehoi.loveyourself@gmail.com>
2024-08-21 17:31:42 +0900

    2. 여러 곳에서 각자가 마음대로 만들어내던 메서드를 Reading이라고 하는 클래스에서 모두 관리하니 이렇게나 깨끗해졌다.

--- ch6/6-9-client3.js
+++ ch6/6-9-client3.js
@@ -1,9 +1,4 @@
-import { acquireReading, baseRate } from './6-9.js';
+import { acquireReading } from './6-9.js';
 
-const aReading = acquireReading();
-
-function calculateBaseCharge(aReading) { // 이거를 지금 6-9-client2.js에서 쓰고 있어서 중복됨
-  return baseRate(aReading.month, aReading.year) * aReading.quantity;
-}
-
-const basicChargeAmount = calculateBaseCharge(aReading);
+const reading = acquireReading();
+console.log(reading.baseCharge);
