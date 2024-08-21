1a26a4a6b174ed474bcb6a15d882e2505674068f
taeyi park <ehoi.loveyourself@gmail.com>
2024-08-21 14:46:02 +0900

    1. 리팩토링 해야할 부분 발견!

--- ch6/6-3-1.js
+++ ch6/6-3-1.js
@@ -1,8 +1,8 @@
 export function price(order) {
   // 가격(price) = 기본가격 - 수량할인 + 배송비
   return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+    order.quantity * order.itemPrice - // 기본가격 
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 + // 수량할인
+    Math.min(order.quantity * order.itemPrice * 0.1, 100) // 배송비
+  ); // 각각 변수화해서 가독성을 높이자
 }
