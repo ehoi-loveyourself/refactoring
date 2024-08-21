9e13fd74ddb20e089ec44d884c903b55c5c24f7a
taeyi park <ehoi.loveyourself@gmail.com>
2024-08-21 16:30:33 +0900

    1. 문제 인식

--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,5 +1,8 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+let defaultOwner = { firstName: '마틴', lastName: '파울러' }; // 1. 이렇게 두면 defaultOwner의 값이 변경된다
 
 export function getDefaultOwner() {
   return defaultOwner;
 }
+
+const result = getDefaultOwner();
+console.log(result);
