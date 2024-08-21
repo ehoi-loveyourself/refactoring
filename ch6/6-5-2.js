77b0d4396bfcbe294302f678de6b2aa140d0cbe7
taeyi park <ehoi.loveyourself@gmail.com>
2024-08-21 15:45:14 +0900

    1. 좋지 않은 메서드 예시

--- ch6/6-5-2.js
+++ ch6/6-5-2.js
@@ -1,10 +1,13 @@
+// [함수 선언 바꾸기]
+// 1. 리팩토링을 어디 해야하는지 모르겠음
+
 export default class Book {
   #reservations;
   constructor() {
     this.#reservations = [];
   }
 
-  addReservation(customer) {
+  addReservation(customer, isPrioirty) { // 2. 이렇게 flag를 통해 우선순위를 가지고 있는지 아닌지에 따라 로직을 다르게 처리하는 식의 메서드는 좋은 것이 아님
     this.#reservations.push(customer);
   }
 
