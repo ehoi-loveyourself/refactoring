9f6cdfb9265a5611e0937f15bc2c9b0394cfaa45
taeyi park <ehoi.loveyourself@gmail.com>
2024-08-21 16:34:12 +0900

    2. 내가 한 리팩토링(그런데 틀렸다)

--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,4 +1,23 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' }; // 1. 이렇게 두면 defaultOwner의 값이 변경된다
+// let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+let defaultOwner = new DefaultOwner('마틴', '파울러');
+
+export class DefaultOwner {
+  #firstName;
+  #lastName;
+
+  constructor(firstName, lastName) {
+    this.#firstName = firstName;
+    this.#lastName = lastName;
+  }
+
+  get firstName() {
+    return this.#firstName;
+  }
+
+   get lastName() {
+    return this.#lastName;f
+   }
+}
 
 export function getDefaultOwner() {
   return defaultOwner;
