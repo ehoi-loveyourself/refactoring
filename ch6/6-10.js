20a78af59e39527d09399c6cf762bf420fd65d00
taeyi park <ehoi.loveyourself@gmail.com>
2024-08-22 09:52:19 +0900

    1. 리팩토링 완료(강의 보고 작성)

--- ch6/6-10.js
+++ ch6/6-10.js
@@ -1,7 +1,27 @@
+// [함수 변환함수로 묶기]
+
+import _ from 'lodash';
+
 const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
 
 export function acquireReading() {
   return reading;
+}
+
+export function enrichReading(original) {
+  // const result = { ...original }; // Object.assign: shallow copy
+  const result = _.cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
+  result.taxableCharge = calculateTaxableCharge(result);
+  return result;
+}
+
+function calculateBaseCharge(reading) {
+  return baseRate(reading.month, reading.year) * reading.quantity;
+}
+
+function calculateTaxableCharge(reading) {
+  return Math.max(0, this.baseCharge - reading.taxThreshold);
 }
 
 export function baseRate(month, year) {
