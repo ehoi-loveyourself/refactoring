32b8fd29b2e165d625a3e922fbd10f44b82b181c
taeyi park <ehoi.loveyourself@gmail.com>
2024-08-22 15:23:00 +0900

    1. 캡슐화 리팩토링

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,29 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+// [불변성]
 
-organization.name = 'Dream Coding';
+class Organization {
+    #name;
+    #country;
+
+    constructor(data) {
+        this.#name = data.name;
+        this.#country = data.country;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    get country() {
+        return this.#country;
+    }
+
+    get rawData() {
+        return { name: this.name, country: this.country };
+    }
+}
+
+const organization = new Organization({ name: 'Acme Gooseberries', country: 'GB' });
+
+organization.name = 'Dream Coding'; // 이렇게 하면 외부에서 함부로 값을 변경할 수 없다!
 console.log(organization.name);
 console.log(organization.country);
