5ce921bcdaf71b257b25e07fd4629b6bd07e2e28
taeyi park <ehoi.loveyourself@gmail.com>
2024-09-10 15:02:11 +0900

    feat: 레코드 캡슐화

--- ch7/7-2.js
+++ ch7/7-2.js
@@ -11,16 +11,19 @@ export class Person {
   }
 
   get courses() {
-    return { ...this.#courses };
+    return [ ...this.#courses ];
   }
 
   addCourse(course) {
     this.#courses.push(course);
   }
 
-  removeCourse(course) {
+  removeCourse(course, runIfAbsent) {
     const index = this.#courses.indexOf(course);
-    if (index === 1) return;
+    if (index === -1) {
+      runIfAbsent();
+      return;
+    }
     this.#courses.splice(index, 1);
   }
 }
@@ -45,6 +48,11 @@ export class Course {
 const ellie = new Person('엘리');
 const course = new Course('리팩토링', true);
 ellie.addCourse(course);
+console.log(ellie.courses.length); // 외부에서 course를 업데이트 하지 못하도록 하였기 때문에 undefined가 나옴
+ellie.removeCourse(course, () => {
+  console.log("해당 강의는 존재하지 않습니다.");
+});
 console.log(ellie.courses.length);
-ellie.removeCourse(course);
-console.log(ellie.courses.length);
+ellie.removeCourse(course, () => {
+  console.log("해당 강의는 존재하지 않습니다.");
+}); // 한번 더 삭제하면 로그가 뜬다.
