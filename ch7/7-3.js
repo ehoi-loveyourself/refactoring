ea2bbe6a2548b4bf4c132eafb9d777e127c5c4da
taeyi park <ehoi.loveyourself@gmail.com>
2024-09-10 15:57:00 +0900

    v2-1: 심화 리팩토링

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -10,7 +10,8 @@ export class Order {
   // 그 때 이렇게 메서드를 한 곳에서 관리해주면 수정하기도 쉽고(유지보수 하기도 쉽고) 
   // 해당 메서드를 여러 곳에서 사용하면 되니까 재사용성도 높아진다.
   isHighPriority() {
-    return 'high' === o.priority || 'rush' === o.priority;
+    // return 'high' === o.priority || 'rush' === o.priority;
+    return this.priority.higherThan(new Priority('normal'));
   }
 }
 
@@ -19,11 +20,25 @@ export class Order {
 class Priority {
   #value;
   constructor(value) {
-    if (Priority.legalValues.contains(value)) {
-      this.value = value;
+    if (Priority.legalValues().includes(value)) {
+      this.#value = value;
     } else {
       throw new Error(`${value} is invalid for Priority.`);
     }
+  }
+
+  // legalValues에서 배열의 index가 커질수록 우선순위가 높아지고 있으므로
+  // 해당 우선순위의 idx를 가져오는 메서드를 하나 만들고
+  get index() {
+    return Priority.legalValues().indexOf(this.#value);
+  }
+
+  equals(other) {
+    return this.index === other.index;
+  }
+
+  higherThan(other) {
+    return this.index > other.index;
   }
 
   // 그런데 아무 value나 넣고 Priority로 생성하면 안되니까 규정을 둬야 해
@@ -44,3 +59,4 @@ const orders = [
 // 4. 그리고 외부에서는 order 객체에서 메서드를 호출해서 쓴다. (나는 필터까지 다 해서 리턴하는 걸 생각했는데
 // 그건 데이터를 가용해서 쓰려는 사람에게 자유를 주어야 하니까 적절하게 리팩토링 하는 것이 중요하다는 생각이 들었다.)
 const highPriorityCount = orders.filter((o) => o.isHighPriority()).length;
+console.log(highPriorityCount);
