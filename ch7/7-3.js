6423ea671adfac3a8ee426fb2f124d32a4b8955c
taeyi park <ehoi.loveyourself@gmail.com>
2024-09-10 15:08:05 +0900

    1. 내 생각

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -2,6 +2,8 @@ export class Order {
   constructor(data) {
     this.priority = data.priority;
   }
+
+  // 여기 Order 클래스 내부에서 highPriorityCount를 구할 수 있도록 메서드를 제공하면 좋을 것 같은데 어떻게 하면 될지 모르겠다.
 }
 
 const orders = [
